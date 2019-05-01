9441469b4dd0d7362f91edfcf530c167cb8bd41f
Matt D'Angeli <MattDAngeli+dev@gmail.com>
2019-04-30 20:41:04 -0400

    Fixed whitespace with source code

--- assignments/openmp/integration/src/trap.c
+++ assignments/openmp/integration/src/trap.c
@@ -23,38 +23,38 @@
 #include <math.h>
 #include <float.h>
 #include <time.h>
+#include <omp.h>
 
 double compute_using_omp (float, float, int, float, int);
 double compute_gold (float, float, int, float);
 
-int 
-main (int argc, char **argv) 
+int main (int argc, char **argv) 
 {
-    if (argc < 5) {
-        printf ("Usage: trap lower-limit upper-limit num-trapezoids num-threads\n");
-        printf ("lower-limit: The lower limit for the integral\n");
-        printf ("upper-limit: The upper limit for the integral\n");
-        printf ("num-trapezoids: Number of trapeziods used to approximate the area under the curve\n");
-        printf ("num-threads: Number of threads to use in the calculation\n");
-        exit (EXIT_FAILURE);
-    }
+   if (argc < 5) {
+      printf ("Usage: trap lower-limit upper-limit num-trapezoids num-threads\n");
+      printf ("lower-limit: The lower limit for the integral\n");
+      printf ("upper-limit: The upper limit for the integral\n");
+      printf ("num-trapezoids: Number of trapeziods used to approximate the area under the curve\n");
+      printf ("num-threads: Number of threads to use in the calculation\n");
+      exit (EXIT_FAILURE);
+   }
 
-    float a = atoi (argv[1]); /* Lower limit. */
-	float b = atof (argv[2]); /* Upper limit. */
-	float n = atof (argv[3]); /* Number of trapezoids. */
+   float a = atoi (argv[1]); /* Lower limit. */
+   float b = atof (argv[2]); /* Upper limit. */
+   float n = atof (argv[3]); /* Number of trapezoids. */
 
-	float h = (b - a)/(float) n; /* Base of each trapezoid. */  
-	printf ("The base of the trapezoid is %f \n", h);
+   float h = (b - a)/(float) n; /* Base of each trapezoid. */  
+   printf ("The base of the trapezoid is %f \n", h);
 
-	double reference = compute_gold (a, b, n, h);
-    printf ("Reference solution computed using single-threaded version = %f \n", reference);
+   double reference = compute_gold (a, b, n, h);
+   printf ("Reference solution computed using single-threaded version = %f \n", reference);
 
-	/* Write this function to complete the trapezoidal rule using omp. */
-    int num_threads = atoi (argv[4]); /* Number of threads. */
-	double omp_result = compute_using_omp (a, b, n, h, num_threads);
-	printf ("Solution computed using %d threads = %f \n", num_threads, omp_result);
+   /* Write this function to complete the trapezoidal rule using omp. */
+   int num_threads = atoi (argv[4]); /* Number of threads. */
+   double omp_result = compute_using_omp (a, b, n, h, num_threads);
+   printf ("Solution computed using %d threads = %f \n", num_threads, omp_result);
 
-    exit(EXIT_SUCCESS);
+   exit(EXIT_SUCCESS);
 } 
 
 /*------------------------------------------------------------------
@@ -64,8 +64,7 @@ main (int argc, char **argv)
  * Output: sqrt((1 + x^2)/(1 + x^4))
 
  */
-float 
-f (float x) 
+float f (float x) 
 {
     return sqrt ((1 + x*x)/(1 + x*x*x*x));
 }
@@ -77,8 +76,7 @@ f (float x)
  * Input args:  a, b, n, h
  * Return val:  Estimate of the integral 
  */
-double 
-compute_gold (float a, float b, int n, float h) 
+double compute_gold (float a, float b, int n, float h) 
 {
    double integral;
    int k;
@@ -97,9 +95,9 @@ compute_gold (float a, float b, int n, float h)
 double 
 compute_using_omp (float a, float b, int n, float h, int num_threads)
 {
-	double integral = 0.0;
+   double integral = 0.0;
 
-    return integral;
+   return integral;
 }
 
 
