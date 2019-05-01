566c8b78ba620d1e6a3ce62fefec413639021010
Matt D'Angeli <MattDAngeli+dev@gmail.com>
2019-04-30 22:13:45 -0400

    Completed OpenMP implementation

--- assignments/openmp/integration/src/trap.c
+++ assignments/openmp/integration/src/trap.c
@@ -1,8 +1,8 @@
 /*  Purpose: Calculate definite integral using trapezoidal rule.
  *
- * Input:   a, b, n, num_threads
+ * Input:   a, b, n_trapezoids, n_threads
  * Output:  Estimate of integral from a to b of f(x)
- *          using n trapezoids, with num_threads.
+ *          using n trapezoids, with n_threads.
  *
  * Compile: gcc -fopenmp -o trap trap.c -O3 -std=c99 -Wall -lm
  * Usage:   ./trap
@@ -22,7 +22,7 @@
 #include <string.h>
 #include <math.h>
 #include <float.h>
-#include <time.h>
+#include <sys/time.h>
 #include <omp.h>
 
 double compute_using_omp (float, float, int, float, int);
@@ -41,18 +41,31 @@ int main (int argc, char **argv)
 
    float a = atoi (argv[1]); /* Lower limit. */
    float b = atof (argv[2]); /* Upper limit. */
-   float n = atof (argv[3]); /* Number of trapezoids. */
+   float n_trapezoids = atof (argv[3]); /* Number of trapezoids. */
 
-   float h = (b - a)/(float) n; /* Base of each trapezoid. */  
+   float h = (b - a)/(float) n_trapezoids; /* Base of each trapezoid. */  
    printf ("The base of the trapezoid is %f \n", h);
 
-   double reference = compute_gold (a, b, n, h);
+   struct timeval start, stop;
+
+   gettimeofday( &start, NULL );
+   double reference = compute_gold (a, b, n_trapezoids, h);
+   gettimeofday( &stop, NULL );
+
    printf ("Reference solution computed using single-threaded version = %f \n", reference);
+   printf("Execution time = %fs.\n", (float)(stop.tv_sec - start.tv_sec +\
+            (stop.tv_usec - start.tv_usec) / (float) 1000000));
 
    /* Write this function to complete the trapezoidal rule using omp. */
-   int num_threads = atoi (argv[4]); /* Number of threads. */
-   double omp_result = compute_using_omp (a, b, n, h, num_threads);
-   printf ("Solution computed using %d threads = %f \n", num_threads, omp_result);
+   int n_threads = atoi (argv[4]); /* Number of threads. */
+
+   gettimeofday( &start, NULL );
+   double omp_result = compute_using_omp (a, b, n_trapezoids, h, n_threads);
+   gettimeofday( &stop, NULL );
+
+   printf ("Solution computed using %d threads = %f \n", n_threads, omp_result);
+   printf("Execution time = %fs.\n", (float)(stop.tv_sec - start.tv_sec +\
+            (stop.tv_usec - start.tv_usec) / (float) 1000000));
 
    exit(EXIT_SUCCESS);
 } 
@@ -73,17 +86,17 @@ float f (float x)
  * Function:    compute_gold
  * Purpose:     Estimate integral from a to b of f using trap rule and
  *              n trapezoids using a single-threaded version
- * Input args:  a, b, n, h
+ * Input args:  a, b, n_trapezoids, h
  * Return val:  Estimate of the integral 
  */
-double compute_gold (float a, float b, int n, float h) 
+double compute_gold (float a, float b, int n_trapezoids, float h) 
 {
    double integral;
    int k;
 
    integral = (f(a) + f(b))/2.0;
 
-   for (k = 1; k <= n-1; k++) 
+   for (k = 1; k <= n_trapezoids-1; k++) 
      integral += f(a+k*h);
    
    integral = integral*h;
@@ -92,10 +105,21 @@ double compute_gold (float a, float b, int n, float h)
 }  
 
 /* FIXME: Complete this function to perform the trapezoidal rule using omp. */
-double 
-compute_using_omp (float a, float b, int n, float h, int num_threads)
+double compute_using_omp (float a, float b, int n_trapezoids, float h, int n_threads)
 {
-   double integral = 0.0;
+   double integral = ( f(a) + f(b) ) / 2.0;
+   int i;
+
+#pragma omp parallel num_threads(n_threads) private(i) reduction(+:integral)
+   {
+#pragma omp for
+      for (i = 1; i <= n_trapezoids - 1; i++) {
+         // printf("[THREAD %d] Adding iteration %d\n", omp_get_thread_num(), i);
+         integral += f(a + i*h);
+      }
+   } // End of OpenMP parallel structure
+
+   integral *= h;
 
    return integral;
 }
