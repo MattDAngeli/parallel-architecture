8f58ba49c4029d98b9ee5a56724c8f55644bee3f
Matt D'Angeli <MattDAngeli+dev@gmail.com>
2019-05-19 17:57:17 -0400

    Fixed input type for free_matrix_on_device

--- assignments/cuda/jacobi-solver/src/jacobi_iteration.h
+++ assignments/cuda/jacobi-solver/src/jacobi_iteration.h
@@ -33,7 +33,7 @@ void print_matrix (const matrix_t);
 float get_random_number (int, int);
 void check_CUDA_error (const char *);
 int check_results (float *, float *, int, float);
-void free_matrix_from device ( matrix_t );
+void free_matrix_from device ( matrix_t * );
 void print_exec_time ( struct timeval, struct timeval );
 
 #endif /* _JACOBI_ITERATION_H_ */
